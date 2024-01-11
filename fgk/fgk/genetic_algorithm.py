df1512efb67d0217a4f788bd5134187059e2bca7
Guoliang He <guolianghe1996@gmail.com>
2024-01-11 10:19:08 +0000

    refactor ga

--- fgk/fgk/genetic_algorithm.py
+++ fgk/fgk/genetic_algorithm.py
@@ -13,61 +13,109 @@ from CuAsm.CubinFile import CubinFile
 
 # mutation
 from fgk.mutator import MutationEngine
-from fgk.sample import Sample, CtrlSample
+from fgk.sample import Sample
 from fgk.utils.logger import get_logger
 from fgk.utils.record import save_data
 
 logger = get_logger(__name__)
 
 
-class GeneticSample(Sample):
+class CtrlSample(Sample):
+    """ only Mutate the control code """
+
+    def get_mutable(self) -> list[int]:
+        lines = []
+        self.actions = []
+        for i, line in enumerate(self.kernel_section):
+            line = line.strip()
+            # skip headers
+            if len(line) > 0 and line[0] == '[':
+                out = self.engine.decode(line)
+                ctrl_code, _, _, _, _, _ = out
+
+                out = self.engine.decode_ctrl_code(ctrl_code)
+                _, _, _, yield_flag, stall_count = out
+                if yield_flag is None or stall_count is None:
+                    continue
+
+                # 1. existing yield flag
+                # if yield_flag == 'Y':
+                #     self.candidates.append(i)
+                #     lines.append(line)
+                # elif int(stall_count[-2]) > 3:
+                #     self.candidates.append(i)
+                #     lines.append(line)
+
+                # 2. consider all flag
+                # TODO: can filter out e.g. NOP?
+                self.candidates.append(i)
+                lines.append(line)
+                if yield_flag == 'Y':
+                    self.actions.append(1)
+                elif yield_flag == '-':
+                    self.actions.append(0)
+                else:
+                    raise RuntimeError(f'invalid yield flag: {yield_flag}')
+
+        # dimension of the optimization problem
+        self.dims = len(self.candidates)
+        return self.candidates
 
     def apply(self, index, action):
         lineno = self.candidates[index]
-        if action == -1:
-            try:
-                self.kernel_section[lineno - 1], self.kernel_section[
-                    lineno] = self.kernel_section[lineno], self.kernel_section[
-                        lineno - 1]
-                self.candidates[index] -= 1
-            except IndexError as e:
-                # it is possible that the index is out of range during mutation
-                print(f'IndexError: {lineno}; {len(self.kernel_section)};')
-        elif action == 1:
-            try:
-                self.kernel_section[lineno], self.kernel_section[
-                    lineno +
-                    1] = self.kernel_section[lineno +
-                                             1], self.kernel_section[lineno]
-                self.candidates[index] += 1
-            except IndexError as e:
-                print(f'IndexError: {lineno}; {len(self.kernel_section)};')
+        line = self.kernel_section[lineno]
+        self.kernel_section[lineno] = self._set_yield_for_line(line, action)
+
+    def _set_yield_for_line(self, line: str, action: int):
+        # NOTE: the line is guranteed to be a valid asm line
+        index = -1
+        for i, char in enumerate(line):
+            if char == 'S':
+                index = i
+                break
+
+        if index == -1:
+            raise RuntimeError(f'invalid line: {line}')
+
+        # print(f'action: {action}')
+        # print(f'before action: {line}')
+        if action == 1:
+            line = line[:index - 2] + 'Y' + line[index - 1:]
         elif action == 0:
-            pass
+            line = line[:index - 2] + '-' + line[index - 1:]
         else:
-            assert False, f'invalid action: {action}'
+            raise RuntimeError(f'invalid action: {action}')
+        # print(f"after action: {line}")
+
+        return line
 
 
 def create_population(
     pop_size: int,
-    init_sample: Sample,
-    sample_ctr: Union[CtrlSample, GeneticSample],
-) -> Union[CtrlSample, GeneticSample]:
+    init_sample: CtrlSample,
+) -> CtrlSample:
     population = []
     for _ in range(pop_size):
-        sample = sample_ctr(init_sample.kernel_section, init_sample.engine)
-        mutable = sample.get_mutable()
-        n = len(mutable)
-        indexes = [i for i in range(n)]
-        # actions = [random.randint(-1, 1) for _ in range(n)]
-        actions = [random.choice([0, 1]) for _ in range(n)]
-        sample.apply_all(indexes, actions)
+        sample = CtrlSample(init_sample.kernel_section, init_sample.engine)
+
+        ## 1. random init
+        # mutable = sample.get_mutable()
+        # n = len(mutable)
+        # indexes = [i for i in range(n)]
+        # actions = [random.choice([0, 1]) for _ in range(n)]
+        # sample.apply_all(indexes, actions)
+
+        ## 2. init from default
+        sample.get_mutable()
         population.append(sample)
     return population
 
 
-def tournament_selection(population: list[Sample], tournament_size,
-                         eng: MutationEngine) -> list[Sample]:
+def tournament_selection(
+    population: list[CtrlSample],
+    tournament_size,
+    eng: MutationEngine,
+) -> list[CtrlSample]:
     selected = []
     for _ in range(len(population)):
         tournament = random.sample(population, tournament_size)
@@ -94,23 +142,20 @@ def mutation(individual, mutation_rate):
 
 
 def build_child(
-    parent: GeneticSample,
+    parent: CtrlSample,
     mutated_action: list[int],
-    sample_ctr: Union[CtrlSample, GeneticSample],
     eng: MutationEngine,
 ):
-    child = sample_ctr(parent.kernel_section, eng)
-    child.candidates = deepcopy(parent.candidates)
-    child.dims = parent.dims
+    child = CtrlSample(parent.kernel_section, eng)
+    child.get_mutable()
     child.apply_all([i for i in range(len(mutated_action))], mutated_action)
     return child
 
 
 def crossover(
-    parent1: GeneticSample,
-    parent2: GeneticSample,
+    parent1: CtrlSample,
+    parent2: CtrlSample,
     mutation_rate: float,
-    sample_ctr: Union[CtrlSample, GeneticSample],
     eng: MutationEngine,
 ):
     n = parent1.dims
@@ -123,8 +168,8 @@ def crossover(
     mutated_action1 = mutation(crossover_action1, mutation_rate)
     mutated_action2 = mutation(crossover_action2, mutation_rate)
 
-    child1 = build_child(parent1, mutated_action1, sample_ctr, eng)
-    child2 = build_child(parent2, mutated_action2, sample_ctr, eng)
+    child1 = build_child(parent1, mutated_action1, eng)
+    child2 = build_child(parent2, mutated_action2, eng)
     return child1, child2
 
 
@@ -135,11 +180,10 @@ def genetic_algorithm(
     generations,
     tournament_size,
     mutation_rate,
-    sample_ctr: Union[CtrlSample, GeneticSample],
     eng: MutationEngine,
 ):
     cmp = lambda x: x if x is not None else float("-inf")
-    population = create_population(population_size, init_sample, sample_ctr)
+    population = create_population(population_size, init_sample)
 
     for generation in range(generations):
         selected_population = tournament_selection(
@@ -160,7 +204,6 @@ def genetic_algorithm(
                 parent1,
                 parent2,
                 mutation_rate,
-                sample_ctr,
                 eng,
             )
             new_population.extend([child1, child2])
@@ -238,13 +281,11 @@ def run_genetic_algorithm(
     )
 
     # ===== start =====
-    # sample = GeneticSample(eng.kernel_section, eng)
     sample = CtrlSample(eng.kernel_section, eng)
     sample.get_mutable()
-    init_perf = max([eng.get_init_perf() for _ in range(5)])
+    init_perf = max([eng.get_init_perf() for _ in range(3)])
     logger.info(f'init perf: {init_perf:.2f}; dims: {sample.dims}')
     if init_perf < 0:
-        # logger.critical(f'init perf {init_perf} < 0; not valid cubin')
         raise RuntimeError(f'init perf {init_perf} < 0; not valid cubin')
 
     _t1 = time.perf_counter()
@@ -255,7 +296,6 @@ def run_genetic_algorithm(
         generations,
         tournament_size,
         mutation_rate,
-        CtrlSample,
         eng,
     )
     _t2 = time.perf_counter()
