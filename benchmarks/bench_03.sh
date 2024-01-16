ac37a7ad7c48eee56bd72c466550ad47f800f00e
Guoliang He <guolianghe1996@gmail.com>
2024-01-16 03:31:44 +0000

    make mm example

---
+++ benchmarks/bench_03.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# config
+start=${1:-1}
+seeds=${2:-100}
+workloads=(
+        512
+        1024
+        2048
+        4096
+        8192
+     )
+
+for seed in $( seq $start $seeds ); do
+        for workload in "${workloads[@]}"; do
+                echo
+                echo "workload ${workload}; seed ${seed}: "
+                echo
+                python benchmarks/03-matrix-multiplication.py \
+                        --seed $seed \
+                        --wl $workload
+        done
+done
