855eebe4345b70962d6c5b1476ae9aa83c4e9e57
Guoliang He <guolianghe1996@gmail.com>
2024-02-22 15:04:59 +0000

    refactor

--- fgk/fgk/__init__.py
+++
@@ -1,7 +0,0 @@
-from simulated_annealing import run_simulated_annealing
-from genetic_algorithm import run_genetic_algorithm
-
-__all__ = [
-    'run_simulated_annealing',
-    'run_genetic_algorithm',
-]
--- fgk/fgk/utils/constant.py
+++ fgk/fgk/utils/constant.py
@@ -3,8 +3,18 @@
 # example sass opcode
 # see : https://docs.nvidia.com/cuda/cuda-binary-utilities/index.html
 
+# adding GPU ISA probably need to add instruction repository
+# https://github.com/cloudcores/CuAssembler/blob/master/UserGuide.md#instruction-assembler-repository
+
 def get_mutatable_ops(cc):
-    if cc == (8, 0):
+    if cc == (8, 9):
+        memory_ops = ['LDG', 'STG', 'LDS', 'LDSM']
+        ban_ops = [
+            'LDGDEPBAR'
+            'DEPBAR',
+        ]
+        return memory_ops, ban_ops
+    elif cc == (8, 0):
         memory_ops = ['LDG', 'STG', 'LDS', 'LDSM']
         ban_ops = [
             'LDGDEPBAR'
